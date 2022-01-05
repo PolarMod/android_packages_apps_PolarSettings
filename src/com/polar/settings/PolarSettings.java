c2dacf81e64aee781724df28a622490c657ca840
Andrewerr <p01ar@p01ar.net>
2022-01-05 18:02:22 +0200

    [custom settings]: Initial commit

---
+++ src/com/polar/settings/PolarSettings.java
@@ -0,0 +1,20 @@
+package com.polar.Settings;
+
+import android.provider.SearchIndexableResource;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class PolarSettings extends SettingsPreferenceFragment{
+
+  @Override
+  public void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public int getMetricsCategory(){
+    return MetricsProto.MetricsEvent.POLAR_SETTINGS;
+  }
+}
