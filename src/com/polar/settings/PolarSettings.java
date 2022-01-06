38f6a8e1eb44ef1ccb768a38f6c6bb0f48b3df73
Andrewerr <p01ar@p01ar.net>
2022-01-06 23:19:07 +0200

    [custom settings][WIP]: adding statusbar

--- src/com/polar/settings/PolarSettings.java
+++ src/com/polar/settings/PolarSettings.java
@@ -1,6 +1,11 @@
 package com.polar.settings;
 
 import android.provider.SearchIndexableResource;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.provider.Settings;
 import android.os.Bundle;
 
 import com.android.settings.SettingsPreferenceFragment;
@@ -12,10 +17,14 @@ public class PolarSettings extends SettingsPreferenceFragment{
   @Override
   public void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
+    addPreferencesFromResource(R.xml.main_menu);
+    setHasOptionsMenu(true);
   }
 
   @Override
   public int getMetricsCategory(){
     return MetricsProto.MetricsEvent.POLAR_SETTINGS;
   }
+
+  //TODO: Make PolarSettings Indexable
 }
