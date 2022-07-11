055aa3c1c43a2c2efe31bff99534c20b8ef0e945
Andrewerr <toddot@protonmail.com>
2022-07-11 20:24:39 +0300

    [custom settings] [FIX]: fixed strings and names

--- src/com/polar/settings/preferences/LineageSecureSettingsStore.java
+++ src/com/polar/settings/preferences/LineageSecureSettingsStore.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.crdroid.settings.preferences;
+package com.polar.settings.preferences;
 
 import android.content.ContentResolver;
 import android.preference.PreferenceDataStore;
